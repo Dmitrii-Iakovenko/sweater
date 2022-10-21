559764bc2e3924ebc305ee02d582778f1b62ccea
Dmitry Yakovenko <jads84@gmail.com>
2022-10-21 09:05:57 +0300

    Add registration messages

--- src/main/java/com/wutreg/sweater/controller/RegistrationController.java
+++ src/main/java/com/wutreg/sweater/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.wutreg.sweater.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -25,11 +27,22 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(
+            @RequestParam("password2") String passwordConfirmation,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model
+    ) {
+
+        boolean isConfirmEmpty = !StringUtils.hasText(passwordConfirmation);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Подтверждение пароля не может быть пустым!");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirmation)) {
             model.addAttribute("password2Error", "Пароли не равны");
         }
-        if (bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
             return "registration";
@@ -47,8 +60,10 @@ public class RegistrationController {
         boolean isActivated = userService.activateUser(code);
 
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "Пользователь успешно активирован");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Код активации не найден!");
         }
 
--- src/main/java/com/wutreg/sweater/entity/User.java
+++ src/main/java/com/wutreg/sweater/entity/User.java
@@ -30,9 +30,6 @@ public class User implements UserDetails {
     @NotBlank(message = "Password cannot be empty")
     private String password;
 
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct")
